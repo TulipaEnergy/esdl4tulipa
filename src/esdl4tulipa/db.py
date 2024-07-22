89b2c9ecdbcf3e4a2e3e462790913e2cdf917b23
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-07-22 16:28:58 +0200

    lint: fix noqa typo

--- src/esdl4tulipa/db.py
+++ src/esdl4tulipa/db.py
@@ -8,7 +8,7 @@ from .mapping import TAssets
 
 def insert_into(con: duckdb.DuckDBPyConnection, tbl: str, records: tuple[TAssets, ...]):
     """Insert Tulipa records into DuckDB."""
-    df = pd.DataFrame(asdict(i) for i in records)  # noqa: F84
+    df = pd.DataFrame(asdict(i) for i in records)  # noqa: F841
     # FIXME: use prepared statements
     con.execute(f"CREATE TABLE {tbl} AS SELECT * FROM df")
     return tbl
